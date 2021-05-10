463e8060922f90715cf0663fd64e4bc8fc7707f9
AM-SAPP <8923499556nikhil@gmail.com>
2021-05-10 05:43:51 +0530

    starter files

---
+++ src/components/MainComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './HomeComponent'
+function Main(){
+    return(
+        <div>
+            <h1>imported Main</h1>
+            <Home/>
+        </div>
+    )
+}
+
+export default Main;
